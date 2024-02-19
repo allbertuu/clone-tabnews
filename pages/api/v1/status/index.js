c86e1fc615e139be7c42971c14c3c53ccd3852e6
Alberto <albertovinicius3@gmail.com>
2024-02-19 17:50:57 -0300

    wip: database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-  return res.status(200).json({ status: "ok parça" });
+import database from "../../../../infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as soma;");
+  console.log(result.rows);
+  return res.status(200).json({ status: "ok amigo" });
 }
 
 export default status;
